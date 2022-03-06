3506f89615573f79d31a3625ee11391b68daceeb
G9000 <leo.caesar@live.com>
2022-03-07 05:13:38 +0800

    update

---
+++ components/EthereumLogo.js
@@ -0,0 +1,80 @@
+const EthereumLogo = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="44"
+      viewBox="0 0 30 44"
+      fill="none"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M28.9975 25.1189L14.9998 43.6836V33.2608L28.9975 25.1189Z"
+        fill="#C8B2F5"
+        stroke="#3441C0"
+        strokeWidth="0.5"
+        strokeMiterlimit="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1.00323 25.1189L15.001 43.6836V33.2608L1.00323 25.1189Z"
+        fill="#EECBC0"
+        stroke="#3441C0"
+        strokeWidth="0.5"
+        strokeMiterlimit="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1.00011 22.9676L15.0005 16.7426V30.9335L1.00011 22.9676Z"
+        fill="#87A9F0"
+        stroke="#3441C0"
+        strokeWidth="0.5"
+        strokeMiterlimit="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M28.9999 22.9676L14.9995 16.7426V30.9335L28.9999 22.9676Z"
+        fill="#CAB3F5"
+        stroke="#3441C0"
+        strokeWidth="0.5"
+        strokeMiterlimit="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1.00323 22.9671L15.001 1V16.7421L1.00323 22.9671Z"
+        fill="#EECBC0"
+        stroke="#3441C0"
+        strokeWidth="0.5"
+        strokeMiterlimit="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M28.9975 22.9671L14.9998 1V16.7421L28.9975 22.9671Z"
+        fill="#B8FBF6"
+        stroke="#3441C0"
+        strokeWidth="0.5"
+        strokeMiterlimit="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+};
+
+export default EthereumLogo;
